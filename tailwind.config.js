01acd53424b8439b1c58822251f14d314d4d9201
svensantifort1-tech <svensantifort1@gmail.com>
2026-03-26 20:06:12 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Tells Tailwind to look at all HTML and JS files in this folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
