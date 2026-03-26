f441f27a132650d0fc32623d8e3aef3bf7ec7f50
svensantifort1-tech <svensantifort1@gmail.com>
2026-03-26 20:11:22 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.{html,js}"], // Tells Tailwind to look at all HTML and JS files in this folder
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
